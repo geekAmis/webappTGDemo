fcd8b399045c74e1ed34fc216f64de8ba3353267
Lilanga <49157841+geekAmis@users.noreply.github.com>
2024-03-02 22:15:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,43 @@
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
 from aiogram.types import Update
-
+import mysql.connector
 import requests
 
-TOKEN = "5955811135:AAGu7qDTCcz-ChbupcI-_CLiKxq2xjEj2OI"
+db_config = {
+    "host": "localhost",
+    "user": "",
+    "password": "",
+    "database": ""
+}
+connection = mysql.connector.connect(**db_config)
+
+if connection.is_connected():
+    cursor = connection.cursor()
+
+    # Запрос данных из таблицы Adminstructure с указанными столбцами
+    query = "SELECT TG_TOKEN, Balance, PAY_API, WebURL, Owners_Tids FROM Adminstructure"
+    cursor.execute(query)
+
+    # Чтение данных из курсора и формирование JSON-строки
+    result = cursor.fetchall()
+    data = []
+    for row in result:
+        data.append({
+            "TG_TOKEN": row[0],
+            "Balance": row[1],
+            "PAY_API": row[2],
+            "WebURL": row[3],
+            "Owners_Tids": row[4]
+        })
+
+    print("JSON-строка:", data)
+
+else:
+    print("Ошибка подключения к базе данных MySQL.")
+    quit()
+
+connection.close()
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
